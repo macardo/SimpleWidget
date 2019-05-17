57f3db9ddb32a751bebd89f2ec5513b1d6c628f9
Macardo <1513355050@qq.com>
2019-05-17 13:43:42 +0800

    button

--- app/src/main/java/com/macardo/simplewidget/ButtonActivity.java
+++ app/src/main/java/com/macardo/simplewidget/ButtonActivity.java
@@ -7,6 +7,8 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -15,6 +17,9 @@ public class ButtonActivity extends AppCompatActivity implements View.OnClickLis
     private ImageButton imageButton;
     private ToggleButton toggleButton;
     private ImageView imageView;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,8 @@ public class ButtonActivity extends AppCompatActivity implements View.OnClickLis
         imageButton = findViewById(R.id.imageButton);
         toggleButton = findViewById(R.id.toggleButton);
         imageView = findViewById(R.id.imageView2);
+        radioGroup = findViewById(R.id.radioGroup);
+        radioButton3 =findViewById(R.id.radioButton3);
 
 /*        //第1种方式：匿名内部类
         button.setOnClickListener(new View.OnClickListener() {
@@ -55,7 +62,30 @@ public class ButtonActivity extends AppCompatActivity implements View.OnClickLis
                 imageView.setBackgroundResource(isChecked?R.drawable.smile:R.drawable.sad);
             }
         });
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.radioButton:
+                        Toast.makeText(ButtonActivity.this,"radioButton1",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radioButton2:
+                        Toast.makeText(ButtonActivity.this,"radioButton2",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+        radioButton3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    Toast.makeText(ButtonActivity.this,"radioButton3",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
 
     @Override
     public void onClick(View v) {
